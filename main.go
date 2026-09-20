a8e90151d12052a2a6ccdb8747d5c77f504735bc
Sanjeevliv <sanjeevsethilive@gmail.com>
2026-09-20 11:20:42 +0530

    feat: implement live packet capture using AF_PACKET raw sockets

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"golang.org/x/sys/unix"
 )
 
 type EthernetFrame struct {
@@ -32,32 +37,91 @@ func formatMAC(mac [6]byte) string {
 	return fmt.Sprintf(
 		"%02x:%02x:%02x:%02x:%02x:%02x",
 		mac[0],
-		mac[1], 
+		mac[1],
 		mac[2],
 		mac[3],
 		mac[4],
 		mac[5],
 	)
-} 
+}
+
+func htons(v uint16) uint16 {
+	return (v << 8) | (v >> 8)
+}
 
 func main() {
-	frame := []byte{
-		0x00, 0x01, 0x02, 0x0a, 0x10, 0xff,
-		0x11, 0x22, 0x33, 0x44, 0x55, 0x66,
-		0x08, 0x00,
-		// Fake payload
-		0x45, 0x00, 0x00, 0x28,
+
+	if len(os.Args) != 2 {
+		log.Fatalf("usage: %s <interface>", os.Args[0])
 	}
 
-	ethernet, err := parseEthernet(frame)
+	iface := os.Args[1]
+	// Converting interface name to interface index
+	ifaceInfo, err := net.InterfaceByName(iface)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatalf("InterfaceByName: %v", err)
+	}
+	ifindex := ifaceInfo.Index
+
+	// Creating a AF_PACKET socket
+	fd, err := unix.Socket(
+		unix.AF_PACKET,
+		unix.SOCK_RAW,
+		int(htons(unix.ETH_P_ALL)),
+	)
+	if err != nil {
+		log.Fatalf("Socket: %v", err)
+	}
+	defer unix.Close(fd)
+
+	// Bind the packet socket to the interface
+	addr := &unix.SockaddrLinklayer{
+		Protocol: htons(unix.ETH_P_ALL),
+		Ifindex:  ifindex,
+	}
+	err = unix.Bind(fd, addr)
+	if err != nil {
+		log.Fatalf("Bind: %v", err)
 	}
 
-	fmt.Printf("Destination: %s\n", formatMAC(ethernet.Destination))
-	fmt.Printf("Source: %s\n", formatMAC(ethernet.Source))
-	fmt.Printf("EtherType: 0x%04x\n", ethernet.EtherType)
-	fmt.Printf("Payload: %x\n", ethernet.Payload)
+	fmt.Println("Waiting for packets...")
 
+	// Reusable buffer for receiving packets
+	buffer := make([]byte, 65535)
+
+	// Capture loop
+	for {
+		// Wait for one packet.
+		n, from, err := unix.Recvfrom(fd, buffer, 0)
+		if err != nil {
+			log.Fatalf("Recvfrom %v", err)
+		}
+
+		fmt.Printf("\nPacket received: %d bytes", n)
+
+		// Inspect AF_PACKET metadata.
+		if sll, ok := from.(*unix.SockaddrLinklayer); ok {
+			fmt.Printf(
+				"ifindex=%d pkttype=%d protocol=0x%04x\n",
+				sll.Ifindex,
+				sll.Pkttype,
+				sll.Protocol,
+			)
+		}
+
+		packet := buffer[:n]
+		frame, err := parseEthernet(packet)
+		if err != nil {
+			log.Printf("parseEthernet: %v", err)
+			continue
+		}
+
+		fmt.Printf("Destination: %s\n", formatMAC(frame.Destination))
+		fmt.Printf("Source: %s\n", formatMAC(frame.Source))
+		fmt.Printf("EtherType: 0x%04x\n", frame.EtherType)
+		fmt.Printf("Payload: %x\n", frame.Payload)
+
+		fmt.Printf("raw bytes: %x\n", packet)
+
+	}
 }
